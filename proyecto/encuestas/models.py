be3e4150b64ea90b33ce7d8487c69b2a49d2f50c
andres <andresgarcia0313@gmail.com>
2018-11-05 20:32:13 -0500

    Se agregan Modelos Pregunta y Elección y se migran cambios sobre sql lite

--- proyecto/encuestas/models.py
+++ proyecto/encuestas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+# https://docs.djangoproject.com/es/2.1/intro/tutorial02/
+
+
+class Pregunta(models.Model):
+    texto_de_la_pregunta = models.CharField(max_length=200)
+    fecha_de_publicacion = models.DateTimeField()
+
+
+class Eleccion(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    texto_de_eleccion = models.CharField(max_length=200)
+    votos=models.IntegerField(default=0)
